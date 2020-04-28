b4af6aa5737ccdc81e96b23abff62f7c1d50a567
Jinen Setpal <jinens8@gmail.com>
2020-04-29 00:41:24 +0530

    added post action bar

--- app/src/main/java/com/elitedom/app/ui/main/PostView.java
+++ app/src/main/java/com/elitedom/app/ui/main/PostView.java
@@ -11,6 +11,7 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.elitedom.app.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.Objects;
 
@@ -19,15 +20,18 @@ public class PostView extends AppCompatActivity {
     private RelativeLayout relativeLayout;
     private TextView mPostTitle;
     private ImageView mPostImage;
+    private FloatingActionButton mChatRoom;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_view);
 
-        relativeLayout = findViewById(R.id.singlecardlayout);
+        relativeLayout = findViewById(R.id.single_card);
         mPostTitle = findViewById(R.id.title);
         mPostImage = findViewById(R.id.postImage);
+        mChatRoom = findViewById(R.id.fab);
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         Objects.requireNonNull(getSupportActionBar()).hide();
@@ -41,5 +45,6 @@ public class PostView extends AppCompatActivity {
         String message = intent.getStringExtra("title");
         mPostTitle.setText(message);
         mPostImage.setClipToOutline(true);
+        mChatRoom.animate().alpha(1.0f);
     }
 }
