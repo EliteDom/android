52bd93ceec08343636e0395ae103962a94c04f9d
jinensetpal <jinens8@gmail.com>
2020-07-17 01:32:43 +0530

    quiz intial layout configured

--- app/src/main/java/com/elitedom/app/ui/dorms/Quiz.java
+++ app/src/main/java/com/elitedom/app/ui/dorms/Quiz.java
@@ -22,7 +22,7 @@ public class Quiz extends AppCompatActivity {
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
         Objects.requireNonNull(getSupportActionBar()).hide();
 
-        ConstraintLayout constraintLayout = findViewById(R.id.quiz_container);
+        ConstraintLayout constraintLayout = findViewById(R.id.quiz_layout);
         AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
         animationDrawable.setEnterFadeDuration(2000);
         animationDrawable.setExitFadeDuration(4000);
