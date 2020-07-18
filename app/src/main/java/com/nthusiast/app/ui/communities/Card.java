7627d9c30206a72fb4595693bb85b99b6db40904
jinensetpal <jinens8@gmail.com>
2020-07-18 21:46:30 +0530

    no followed dorms bug fixed

--- app/src/main/java/com/nthusiast/app/ui/communities/Card.java
+++ app/src/main/java/com/nthusiast/app/ui/communities/Card.java
@@ -2,15 +2,20 @@ package com.nthusiast.app.ui.communities;
 
 import android.net.Uri;
 
-class Cards {
+class Card {
     private final Uri imageResource;
     private String title;
     private String info;
 
-    Cards(String title, String info, Uri imageResource) {
+    Card(String title, String info, Uri imageResource) {
         this.title = title;
         this.info = info;
         this.imageResource = imageResource;
+    }
+
+    Card(String text) {
+        this.title = text;
+        this.imageResource = Uri.parse("");
     }
 
     String getTitle() {
