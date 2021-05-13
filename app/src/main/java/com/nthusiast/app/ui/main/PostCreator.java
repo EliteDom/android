0704b8ad07d0746a90dc04d443e1179f2e3601c1
Dhrishaj Garg <dhrishaj.garg@gmail.com>
2021-05-13 14:01:09 +0530

    removed points system

--- app/src/main/java/com/nthusiast/app/ui/main/PostCreator.java
+++ app/src/main/java/com/nthusiast/app/ui/main/PostCreator.java
@@ -8,7 +8,6 @@ import android.graphics.Color;
 import android.graphics.drawable.AnimationDrawable;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
